e85889c42525a5fb80c1d2536b93e75c04a45c86
A-8 <A-8@A-8>
2024-06-03 11:12:09 +0700

    menambah class dan package

---
+++ PBOJava9/src/main/java/com/mycompany/pbojava9/ui/textUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbojava9.ui;
+
+/**
+ *
+ * @author A-8
+ */
+//import com.mycompany.pbojava9;
+//import java.util.Scanner;
+
+public class textUI {
+    public static void main(String[] args) {
+        textUI ui = new textUI();
+        //ui.run();
+    }
+}
